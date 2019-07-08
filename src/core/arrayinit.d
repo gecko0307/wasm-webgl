0408efd653aa54dd3d43979e328596b6a2c0eff4
TimurGafarov <gt@smarthead.ru>
2019-07-08 10:21:20 +0300

    Support latest LDC

--- src/core/arrayinit.d
+++ src/core/arrayinit.d
@@ -2,15 +2,12 @@ module core.arrayinit;
 
 extern(C):
 
-// for array cast
-size_t _d_array_cast_len(size_t len, size_t elemsz, size_t newelemsz)
-{
-    if (newelemsz == 1) {
-        return len*elemsz;
-    }
-    else if ((len*elemsz) % newelemsz) {
-		while(1){}
-    }
-    return (len*elemsz)/newelemsz;
+@trusted nothrow
+size_t _d_arraycast_len(size_t len, size_t elemsz, size_t newelemsz) {
+    const size = len * elemsz;
+    const newlen = size / newelemsz;
+    if (newlen * newelemsz != size)
+        assert(0);
+    return newlen;
 }
 
