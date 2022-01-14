07610806ee4120d7ccc6a1483c64b44774a6ea2e
Timur Gafarov <gecko0307@gmail.com>
2022-01-15 01:14:13 +0300

    Formatting

--- src/wasmrt/math.d
+++ src/wasmrt/math.d
@@ -42,14 +42,14 @@ double sqrt(double number)
 
 double rsqrt(double number)
 {
-	long i;
-	float x2, y;
-	const float threehalfs = 1.5f;
-	x2 = number * 0.5f;
-	y  = number;
-	i  = * cast(long*)&y;
-	i  = 0x5f3759df - (i >> 1);
-	y  = * cast(float*)&i;
-	y  = y * (threehalfs - (x2 * y * y));
-	return y;
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5f;
+    x2 = number * 0.5f;
+    y  = number;
+    i  = * cast(long*)&y;
+    i  = 0x5f3759df - (i >> 1);
+    y  = * cast(float*)&i;
+    y  = y * (threehalfs - (x2 * y * y));
+    return y;
 }
